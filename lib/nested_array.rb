443629144f58b3a66f331c7c27ce0c3c28fc6065
Matt Bugay <mjbugay@yahoo.com>
2020-09-21 17:18:08 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -32,6 +32,6 @@ end
 
 def matrix_update(matrix, row, column, new_value)
   matrix = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
-  result == matrix[row][column][new_value]
+  result = matrix[row][column][new_value]
   return result
 end
